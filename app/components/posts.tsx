65eeeffde30bc7f1b17933b62e03f51dfb1ac1ad
Ryan Marten <ryanmarten2000@gmail.com>
2025-01-28 01:01:40 -0800

    dates

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -21,8 +21,8 @@ export function BlogPosts() {
             className="flex flex-col space-y-1 mb-4"
             href={`/blog/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
+            <div className="w-full flex flex-col md:flex-row">
+              <p className="text-neutral-600 dark:text-neutral-400 min-w-[140px] tabular-nums">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
               <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
