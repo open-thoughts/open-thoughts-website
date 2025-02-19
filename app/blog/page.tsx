6c283fba93c2095f44f8e16af6b29e9dbd702d21
Maheswaran Sathiamoorthy <callsmahesh@gmail.com>
2025-02-18 16:15:50 -0800

    Also add mailchimp to blog home page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,5 @@
 import { BlogPosts } from 'app/components/posts'
-
+import { MailchimpForm } from 'app/components/MailchimpForm'
 export const metadata = {
   description: 'Read my blog.',
 }
@@ -8,6 +8,9 @@ export default function Page() {
   return (
     <section className="mt-8">
       <BlogPosts />
+      <div className="mt-16 border-t border-neutral-200 dark:border-neutral-800 pt-8">
+        <MailchimpForm />
+      </div>
     </section>
   )
 }
